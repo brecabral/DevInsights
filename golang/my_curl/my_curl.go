97db6695e336cca854c70818bf63d39efe1c48f3
Breno <breno.ara.cabral@gmail.com>
2024-10-30 10:51:43 -0300

    feat: add `time elapsed` in `my_curl`

--- golang/my_curl/my_curl.go
+++ golang/my_curl/my_curl.go
@@ -6,24 +6,36 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
+	start := time.Now()
+	ch := make(chan string)
 	for _, url := range os.Args[1:] {
 		if !strings.HasPrefix(url, "http://") {
 			url = "http://" + url
 		}
-		resp, err := http.Get(url)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: %v \n", err)
-			os.Exit(1)
-		}
-		_, err = io.Copy(os.Stdout, resp.Body)
-		resp.Body.Close()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "reading %s: %v \n", url, err)
-			os.Exit(1)
-		}
-		fmt.Printf("%v", resp.Status)
+		go fetch(url, ch)
 	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprintf("fetch: %s error: %v", url, err)
+		return
+	}
+	numBytes, err := io.Copy(io.Discard, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("reading: %s error: %v", url, err)
+		return
+	}
+	ch <- fmt.Sprintf("time: %.2fs bytes: %7d url: %s", time.Since(start).Seconds(), numBytes, url)
 }
